71244a3e8e5109dad00fda27a832fca6c809ce2c
Todd Hansberger <thansber@yahoo.com>
2011-12-05 19:48:34 -0500

    - fix town transition animation

--- js/data/map-transitions.js
+++ js/data/map-transitions.js
@@ -1,8 +1,8 @@
-new Map.Transition(Map.WORLD_MAP, {y:160, x:151}, Map.CONERIA, {y:24, x:17});
-new Map.Transition(Map.WORLD_MAP, {y:161, x:151}, Map.CONERIA, {y:24, x:17});
-new Map.Transition(Map.WORLD_MAP, {y:162, x:151}, Map.CONERIA, {y:24, x:17});
-new Map.Transition(Map.WORLD_MAP, {y:160, x:154}, Map.CONERIA, {y:24, x:17});
-new Map.Transition(Map.WORLD_MAP, {y:161, x:154}, Map.CONERIA, {y:24, x:17});
-new Map.Transition(Map.WORLD_MAP, {y:162, x:154}, Map.CONERIA, {y:24, x:17});
+new Map.Transition(Map.WORLD_MAP, {y:160, x:151}, Map.CONERIA, {y:23, x:16});
+new Map.Transition(Map.WORLD_MAP, {y:161, x:151}, Map.CONERIA, {y:23, x:16});
+new Map.Transition(Map.WORLD_MAP, {y:162, x:151}, Map.CONERIA, {y:23, x:16});
+new Map.Transition(Map.WORLD_MAP, {y:160, x:154}, Map.CONERIA, {y:23, x:16});
+new Map.Transition(Map.WORLD_MAP, {y:161, x:154}, Map.CONERIA, {y:23, x:16});
+new Map.Transition(Map.WORLD_MAP, {y:162, x:154}, Map.CONERIA, {y:23, x:16});
 
 //new Map.Transition(Map.CONERIA, {y:25, x:18}, Map.WORLD_MAP, {y:162, x:152});
