eca3702bd55eca51980a8c86e43e2c67b2eab36f
thansber <thansber@yahoo.com>
2013-07-29 12:50:20 -0400

    clean up logging

--- js/battle.js
+++ js/battle.js
@@ -3,20 +3,21 @@ define( /* Battle */
 function($, Logger, Monster, Party, RNG) {
 
   var restrictions = {small:9, large:4, fiend:1, chaos:1, mixed:{small:6, large:2, fiend:0, chaos:0}};
-  
+  var LOG_ID = "Battle";
+
   var calculateSurprise = function(battle) {
-    
+
     if (!battle.runnable) {
-      Logger.debug("encounter is not runnable, normal battle");
+      Logger.debug(LOG_ID, "encounter is not runnable, normal battle");
       return;
     }
-    
-    var leader = null; 
-    $.each(Party.getChars(), function(i, char) { 
-      if (char.isAlive()) { 
+
+    var leader = null;
+    $.each(Party.getChars(), function(i, char) {
+      if (char.isAlive()) {
         leader = char;
         return false;
-      } 
+      }
     });
     var leaderQuickness = Math.floor((leader.agility + leader.luck) / 8);
     var r = RNG.randomUpTo(100, leaderQuickness);
@@ -24,17 +25,17 @@ function($, Logger, Monster, Party, RNG) {
     if (result < 0) {
       result = 0;
     }
-    
-    Logger.info("SURPRISE = (leader quick + random - surprise) " + leaderQuickness + " + " + r + " - " + battle.surprise + " = " + result);
-        
-    return result;    
+
+    Logger.info(LOG_ID, "SURPRISE = (leader quick + random - surprise) " + leaderQuickness + " + " + r + " - " + battle.surprise + " = " + result);
+
+    return result;
   };
-  
+
   var isSetupValid = function(battle) {
     var enemiesBySize = battle.enemiesBySize;
     if (battle.isMixedSize()) {
-      var isValid = 
-        enemiesBySize.small.enemies.length <= restrictions.mixed.small && 
+      var isValid =
+        enemiesBySize.small.enemies.length <= restrictions.mixed.small &&
         enemiesBySize.large.enemies.length <= restrictions.mixed.large &&
         enemiesBySize.fiend.enemies.length <= restrictions.mixed.fiend;
       if (!isValid) {
@@ -42,7 +43,7 @@ function($, Logger, Monster, Party, RNG) {
       }
       return isValid;
     }
-    
+
     for (var s in enemiesBySize) {
       if (enemiesBySize[s].enemies.length > restrictions[s]) {
         alert("Too many " + s  + " enemies [" + enemiesBySize[s].enemies.length + "], must be less than [" + restrictions[s] + "]");
@@ -51,29 +52,29 @@ function($, Logger, Monster, Party, RNG) {
     }
     return true;
   };
-  
+
   var setupEnemiesByName = function(enemyQuantities) {
     if (!enemyQuantities || $.isEmptyObject(enemyQuantities)) {
       return false;
     }
-    
+
     var enemiesByName = {};
     $.each(enemyQuantities, function(i, enemyObj) {
       var enemy = Monster.lookup(enemyObj.name);
       var enemyQty = enemyObj.qty == null ? 1 : enemyObj.qty;
-      
+
       if (!enemiesByName[enemy.getName()]) {
         enemiesByName[enemy.getName()] = [];
       }
-      
+
       for (var i = 0; i < enemyQty; i++) {
         enemiesByName[enemy.getName()].push(Monster.createForBattle(enemy));
       }
     });
-    
+
     return enemiesByName;
   };
-  
+
   var setupEnemiesBySize = function(enemyQuantities) {
     if (!enemyQuantities || $.isEmptyObject(enemyQuantities)) {
       return false;
@@ -86,10 +87,10 @@ function($, Logger, Monster, Party, RNG) {
         sizeCounts[enemy.size].enemies.push(enemyObj.name);
       }
     });
-    
+
     return sizeCounts;
   };
-  
+
   var Battle = function(opt) {
     opt = opt || {};
     this.party = opt.party || {};
@@ -103,19 +104,19 @@ function($, Logger, Monster, Party, RNG) {
     this.isStarted = false;
     this.runnable = opt.runnable == null ? true : opt.runnable;
     this.moveFirstChar = !opt.doNotMove;
-    
+
     var surpriseResult = calculateSurprise(this);
     if (surpriseResult <= 10) {
       this.ambush = true;
     } else if (surpriseResult >= 90) {
       this.preemptive = true;
     }
-    
+
     if (!isSetupValid(this)) {
       return null;
     }
   };
-  
+
   Battle.prototype.calculateRewards = function() {
     var totalExp = 0, goldToAdd = 0;
     for (var name in this.enemiesByName) {
@@ -128,25 +129,25 @@ function($, Logger, Monster, Party, RNG) {
         }
       }
     }
-    
+
     var charsGettingExp = [];
     $.each(Party.getChars(), function(i, char) {
       if (char.isAlive()) { charsGettingExp.push(char); }
     });
-    
+
     var expPerChar = Math.floor(totalExp / charsGettingExp.length);
     // EXP is always at least 1, even if all enemies run away
-    expPerChar = expPerChar <= 0 ? 1 : expPerChar; 
-    
+    expPerChar = expPerChar <= 0 ? 1 : expPerChar;
+
     return { exp:expPerChar, gold:goldToAdd };
   };
   Battle.prototype.getRestrictions = function() { return this.isMixedSize() ? restrictions.mixed : restrictions; };
   Battle.prototype.isAmbush = function() { return !this.isStarted && this.ambush; };
   Battle.prototype.isMixedSize = function() {
     var numSizes = 0;
-    if (this.enemiesBySize.small.enemies.length > 0) { numSizes++; } 
-    if (this.enemiesBySize.large.enemies.length > 0) { numSizes++; }
-    if (this.enemiesBySize.fiend.enemies.length > 0) { numSizes++; }
+    if (this.enemiesBySize.small && this.enemiesBySize.small.enemies.length > 0) { numSizes++; }
+    if (this.enemiesBySize.large && this.enemiesBySize.large.enemies.length > 0) { numSizes++; }
+    if (this.enemiesBySize.fiend && this.enemiesBySize.fiend.enemies.length > 0) { numSizes++; }
     return numSizes > 1;
   };
   Battle.prototype.isPreemptive = function() { return !this.isStarted && this.preemptive; };
@@ -159,7 +160,7 @@ function($, Logger, Monster, Party, RNG) {
     return null;
   };
   Battle.prototype.started = function() { this.isStarted = true; };
-  
+
   return {
     create : function(opt) { return new Battle(opt); }
   };
